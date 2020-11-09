1c22258c49a42b3aed478ca3633366278fc166ae
srinivasaraodannana <srinivasarao.dannana2000@gmail.com>
2020-11-09 20:40:14 +0530

    compleeyjgw

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function playaudio(){
+
+
+var mlp= document.getElementById("ram");
+mlp.currentTime = 7;  
+mlp.play();
+   }
+   function pauseaudio(){
+var mlp= document.getElementById("ram");
+mlp.pause();  
+   }
